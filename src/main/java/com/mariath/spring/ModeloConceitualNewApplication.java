0f2308456b3664c833b3189c5bb073d738c754e6
Marcelo Mariath <gedom2012@gmail.com>
2020-02-26 22:38:43 +0000

    Criação da estrutura basica

---
+++ src/main/java/com/mariath/spring/ModeloConceitualNewApplication.java
@@ -0,0 +1,13 @@
+package com.mariath.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloConceitualNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloConceitualNewApplication.class, args);
+	}
+
+}
